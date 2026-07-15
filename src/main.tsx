245b0b624a9b427d4472eecd28bc1367e3b01e85
yapweijun1996 <yapweijun1996@gmail.com>
2026-07-15 14:15:47 +0800

    style: standardize typography hierarchy

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './typography.css'
 
 const root = document.getElementById('root')
 
